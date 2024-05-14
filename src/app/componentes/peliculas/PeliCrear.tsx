787639eb9dbdd2a11feb5a7106d32956962c81a1
stevan23k <esteproo@gmail.com>
2024-05-14 17:48:28 -0500

    martes 14-05

--- src/app/componentes/peliculas/PeliCrear.tsx
+++ src/app/componentes/peliculas/PeliCrear.tsx
@@ -1,7 +1,36 @@
 import Form from "react-bootstrap/Form";
 import noFoto from "../../../assets/img/noDisponible.png";
+import { useState } from "react";
+import { PeliculaGenero } from "../../modelos/PeliculaGenero";
+import { ARREGLO_PELICULA_GENERO } from "../../utilidades/dominios/DomGenero";
+import { useFormulario } from "../../utilidades/misHooks/useFormulario";
+import { Pelicula } from "../../modelos/Pelicula";
+import { useNavigate } from "react-router-dom";
+import { ARREGLO_PELICULAS } from "../../mocks/Pelicula-mocks";
 
 export const PeliCrear = () => {
+  
+  const irsePara = useNavigate();
+  
+  type formHtml = React.FormEvent<HTMLFormElement>;
+  const [enProceso, setEnProceso] = useState<boolean>(false);
+  const [imgBase64, setImgBase64] = useState<any>();
+  const [imgMiniatura, setImgMiniature] = useState<any>(noFoto);
+
+  const [arrPeliculas] = useState<Pelicula[]>(ARREGLO_PELICULAS);
+  const [arrGenero] = useState<PeliculaGenero[]>(ARREGLO_PELICULA_GENERO);
+
+  let{
+    nombrePelicula,
+    protagonistaPelicula,
+    codGeneroPelicula,
+    imagenPelicula,
+    dobleEnlace,
+    objeto,
+  } = useFormulario<Pelicula>(new Pelicula(0,"","","","",""));
+
+  
+
   return (
     <div className="d-flex justify-content-center">
       <div className="col-md-5 mt-5 pb-4">
@@ -13,11 +42,11 @@ export const PeliCrear = () => {
 
             <div className="card-body">
               <div className="mb-3">
-                <Form.Group controlId="nombrePelicula">
+                <Form.Group controlId="nom">
                   <Form.Label>
                     <span className="rojito">*</span> Nombre película
                   </Form.Label>
-                  <Form.Control size="sm" required type="text" name="nombrePelicula" />
+                  <Form.Control size="sm" required type="text" name="nombrePelicula" value={nombrePelicula} onChange={dobleEnlace}/>
                 </Form.Group>
               </div>
 
@@ -26,18 +55,24 @@ export const PeliCrear = () => {
                   <Form.Label>
                     <span className="rojito">*</span> Protagonista
                   </Form.Label>
-                  <Form.Control size="sm" required type="text" name="protagonistaPelicula" />
+                  <Form.Control size="sm" required type="text" name="protagonistaPelicula" value={protagonistaPelicula} onChange={dobleEnlace}/>
                 </Form.Group>
               </div>
 
               <div className="mb-3">
                 <Form.Group controlId="gen">
                   <Form.Label>
-                    <span className="rojito">*</span> Protagonista
+                    <span className="rojito">*</span> Género
                   </Form.Label>
 
-                  <Form.Select size="sm" required name="generoPelicula">
+                  <Form.Select size="sm" required name="codGeneroPelicula" value={codGeneroPelicula} onChange={dobleEnlace} >
                     <option value="">Seleccione un genero</option>
+
+                    {arrGenero.map((miGenero: PeliculaGenero) => (
+                      <option value={miGenero.codGenero} key={miGenero.codGenero}>
+                        {miGenero.nombreGenero}
+                      </option>
+                    ))}
                   </Form.Select>
                 </Form.Group>
               </div>
